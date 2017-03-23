4ad9f3438ea4eab7ec9e42c3eb823d51663ea1d6
Richard Smiley <smiley@callrail.com>
2017-03-23 00:10:29 -0400

    config stuff starting

--- config.js
+++ config.js
@@ -1,28 +1,42 @@
-console.log("Hello");
+var LAMBDA_NAME = "ses-forwarder-dev-sesForwarder";
+var MAPPING_FILE_BUCKET = "mail.hotdonuts.info";
+var MAPPING_FILE_NAME = "mailmap.json";
 
-var Menu = require('terminal-menu');
-var menu = Menu({ width: 29, x: 4, y: 2 });
-menu.reset();
-menu.write('SERIOUS BUSINESS TERMINAL\n');
-menu.write('-------------------------\n');
+function updateAws() {
+  var aws = require('aws-sdk');
+  aws.config.update({ region: 'us-east-1' });
+  var lambda = new aws.Lambda({ apiVersion: '2015-03-31' });
+  var results = lambda.getFunction({ FunctionName: LAMBDA_NAME }, function(err) { debugger; }, function(err, results) { debugger; });
+  debugger;
+}
 
-menu.add('ADD TRANSACTION INVOICE');
-menu.add('BUSINESS INTELLIGENCE');
-menu.add('ACCOUNTS PAYABLE');
-menu.add('LEDGER BOOKINGS');
-menu.add('INDICATOR CHART METRICS');
-menu.add('BACKUP DATA TO FLOPPY DISK');
-menu.add('RESTORE FROM FLOPPY DISK');
-menu.add('EXIT');
+updateAws();
 
-menu.on('select', function (label) {
-    menu.close();
-    console.log('SELECTED: ' + label);
-});
-process.stdin.pipe(menu.createStream()).pipe(process.stdout);
+function showMenu() {
+  var Menu = require('terminal-menu');
+  var menu = Menu({ width: 29, x: 4, y: 2 });
+  menu.reset();
+  menu.write('MAIL302\n');
+  menu.write('-------------------------\n');
 
-process.stdin.setRawMode(true);
-menu.on('close', function () {
-    process.stdin.setRawMode(false);
-    process.stdin.end();
-});
+  menu.add('MANAGE EMAIL ADDRESSES');
+  menu.add('SYNC FROM AWS');
+  menu.add('UPDATE AWS');
+  menu.add('EXIT');
+
+  menu.on('select', function (label) {
+      menu.close();
+      if (label == "SYNC FROM AWS") {
+        syncFromAws();
+      }
+  });
+  process.stdin.pipe(menu.createStream()).pipe(process.stdout);
+
+  process.stdin.setRawMode(true);
+  menu.on('close', function () {
+      process.stdin.setRawMode(false);
+      process.stdin.end();
+  });
+}
+
+
